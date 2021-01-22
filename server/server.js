76bcb7891a6296b9430eb4e01416475e9040677d
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-01-21 20:16:18 -0500

    minor tweak

--- server/server.js
+++ server/server.js
@@ -6,16 +6,16 @@ import dotenv  from "dotenv";
 
 dotenv.config();
 
-const app = express();
-app.use(bodyParser.json({ limit: "30mb", exceeded: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+const server = express();
+server.use(bodyParser.json({ limit: "30mb", exceeded: true }));
+server.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+server.use(cors());
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Mongo Connection successful. Server running on port: ${PORT}`)))
+    .then(() => server.listen(PORT, () => console.log(`Mongo Connection successful. Server running on port: ${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
