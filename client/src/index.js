9fae20efcdee024435860e8309b441943cb176b3
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-01-25 23:06:06 -0500

    minor tweaks

--- client/src/index.js
+++ client/src/index.js
@@ -5,22 +5,15 @@ import { createStore, applyMiddleware, compose }from 'redux';
 import thunk from 'redux-thunk';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 import reducers from './reducers';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
   <Provider store={store}>
     <App />
-  </Provider>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
